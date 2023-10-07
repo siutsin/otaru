5a41710308419a9a7acef2d6871660f87027f1ce
Simon Li <904442+siutsin@users.noreply.github.com>
2023-10-07 19:57:23 +0100

    fix healthcheck (#16)

--- applications/healthcheck/healthcheck.sh
+++ applications/healthcheck/healthcheck.sh
@@ -2,18 +2,10 @@
 
 set -euxo pipefail
 
-retry=0
-max_retries=3
+response=$(curl -s -o /dev/null -w "%{http_code}" -m 5 "$TARGET_URL")
 
-while [[ $retry -lt $max_retries ]]; do
-  status_code=$(curl -s -o /dev/null -w "%{http_code}" -m 5 "$TARGET_URL")
-
-  if [[ $status_code -eq 200 ]]; then
-    curl -s -m 5 "$HEALTH_CHECK_IO_SUCCESS_URL"
-    exit 0
-  fi
-
-  ((retry++))
-done
-
-curl -s -m 5 "$HEALTH_CHECK_IO_FAILURE_URL"
+if [ "$response" -eq 200 ]; then
+  curl -s -m 5 "$HEALTH_CHECK_IO_SUCCESS_URL"
+else
+  curl -s -m 5 "$HEALTH_CHECK_IO_FAILURE_URL"
+fi
