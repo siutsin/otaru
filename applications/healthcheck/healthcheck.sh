babc5d1faa3a747a0e92d88d463561eefca946f9
Simon Li <li@siutsin.com>
2023-10-07 19:42:43 +0100

    fic healthcheck

--- applications/healthcheck/healthcheck.sh
+++ applications/healthcheck/healthcheck.sh
@@ -6,14 +6,14 @@ retry=0
 max_retries=3
 
 while [[ $retry -lt $max_retries ]]; do
-  status_code=$(curl -s -o /dev/null -w "%{http_code}" -m 2 "$TARGET_URL")
+  status_code=$(curl -s -o /dev/null -w "%{http_code}" -m 5 "$TARGET_URL")
 
   if [[ $status_code -eq 200 ]]; then
-    curl -s "$HEALTH_CHECK_IO_SUCCESS_URL"
+    curl -s -m 5 "$HEALTH_CHECK_IO_SUCCESS_URL"
     exit 0
   fi
 
   ((retry++))
 done
 
-curl -s "$HEALTH_CHECK_IO_FAILURE_URL"
+curl -s -m 5 "$HEALTH_CHECK_IO_FAILURE_URL"
