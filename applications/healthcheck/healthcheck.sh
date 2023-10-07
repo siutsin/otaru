bfd4ca957b200cff9be1dfd07df05d455b26d5d9
Simon Li <li@siutsin.com>
2023-10-07 14:43:21 +0100

    add retry to health check

--- applications/healthcheck/healthcheck.sh
+++ applications/healthcheck/healthcheck.sh
@@ -2,8 +2,15 @@
 
 set -euxo pipefail
 
-if curl -s -i -m 2 "$TARGET_URL"; then
-  curl -s "$HEALTH_CHECK_IO_SUCCESS_URL"
-else
-  curl -s "$HEALTH_CHECK_IO_FAILURE_URL"
-fi
+retry=0
+max_retries=3
+
+while [[ $retry -lt $max_retries ]]; do
+  if curl -s -i -m 2 "$TARGET_URL"; then
+    curl -s "$HEALTH_CHECK_IO_SUCCESS_URL"
+    exit 0
+  fi
+  ((retry++))
+done
+
+curl -s "$HEALTH_CHECK_IO_FAILURE_URL"
