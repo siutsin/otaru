bc448486a7bcc7414294afeb4eb99ec6fac4b067
Simon Li <li@siutsin.com>
2023-10-19 22:11:14 +0100

    split cluster ip and github ip

--- infrastructure/modules/cloudflare-access/02-policy.tf
+++ infrastructure/modules/cloudflare-access/02-policy.tf
@@ -1,14 +1,23 @@
 resource "cloudflare_access_policy" "this" {
   application_id = cloudflare_access_application.this.id
   zone_id        = var.zone_id
-  name           = "IP List"
+  name           = "Cluster IP List"
   precedence     = "1"
   decision       = "bypass"
 
   include {
-    ip = concat(
-      var.ip_list,
-      data.github_ip_ranges.this.hooks
-    )
+    ip = var.ip_list
+  }
+}
+
+resource "cloudflare_access_policy" "github" {
+  application_id = cloudflare_access_application.this.id
+  zone_id        = var.zone_id
+  name           = "GitHub IP List"
+  precedence     = "2"
+  decision       = "bypass"
+
+  include {
+    ip = data.github_ip_ranges.this.hooks
   }
 }
