04f71fe4cd5443b471458a50acae0c0f98fdee99
Simon Li <904442+siutsin@users.noreply.github.com>
2023-12-27 02:28:18 +0000

    migrate jung2bot dev (#163)

--- infrastructure/modules/cloudflare-access/02-policy.tf
+++ infrastructure/modules/cloudflare-access/02-policy.tf
@@ -2,7 +2,14 @@ locals {
   #  https://uptimerobot.com/help/locations/
   #  https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt
   uptime_robot_ip_raw       = split("\n", chomp(file("${path.module}/ip/uptime-robot-ipv4-ipv6.txt")))
-  uptime_robot_ip_addresses = [for ip in local.uptime_robot_ip_raw : trimspace(trimprefix(ip, "\r"))]
+  uptime_robot_ip_addresses = [
+    for ip in local.uptime_robot_ip_raw :
+    format("%s/%s", trimspace(trimprefix(ip, "\r")), strcontains(ip, ".") ? "32" : "128")
+  ]
+
+  #  https://core.telegram.org/resources/cidr.txt
+  telegram_ip_raw       = split("\n", chomp(file("${path.module}/ip/telegram-cidr.txt")))
+  telegram_ip_addresses = [for ip in local.telegram_ip_raw : trimspace(trimprefix(ip, "\r"))]
 }
 
 resource "cloudflare_access_policy" "this" {
@@ -38,5 +45,17 @@ resource "cloudflare_access_policy" "uptime_robot" {
 
   include {
     ip = local.uptime_robot_ip_addresses
+  }
+}
+
+resource "cloudflare_access_policy" "telegram" {
+  application_id = cloudflare_access_application.this.id
+  zone_id        = var.zone_id
+  name           = "Telegram IP List"
+  precedence     = "4"
+  decision       = "bypass"
+
+  include {
+    ip = local.telegram_ip_addresses
   }
 }
