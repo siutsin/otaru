913391ed2e5c5d1e5ef965b37a67b837b38fd338
Simon Li <li@siutsin.com>
2024-10-06 18:39:09 +0100

    add vlan

--- infrastructure/modules/unifi/00-vars.tf
+++ infrastructure/modules/unifi/00-vars.tf
@@ -19,10 +19,31 @@ variable "device_wifi" {
 variable "wan" {
   type = map(object({
     name             = string
-    purpose          = string
+    purpose          = optional(string, "wan")
     wan_dns          = list(string)
     wan_networkgroup = string
     wan_type         = string
     wan_type_v6      = string
+  }))
+}
+
+variable "vlan" {
+  type = map(object({
+    name                   = string
+    purpose                = optional(string, "corporate")
+    dhcp_enabled           = optional(bool, true)
+    dhcp_start             = string
+    dhcp_stop              = string
+    dhcp_v6_start          = optional(string, "::2")
+    dhcp_v6_stop           = optional(string, "::7d1")
+    domain_name            = string
+    ipv6_interface_type    = optional(string, "none")
+    ipv6_pd_start          = optional(string, "::2")
+    ipv6_pd_stop           = optional(string, "::7d1")
+    ipv6_ra_enable         = optional(bool, true)
+    ipv6_ra_priority       = optional(string, "high")
+    ipv6_ra_valid_lifetime = optional(number, 0)
+    multicast_dns          = optional(bool, true)
+    subnet                 = string
   }))
 }
