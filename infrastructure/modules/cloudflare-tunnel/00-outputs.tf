8031f22079bd4d5e95a57fade06e470ab4a5dc15
Simon Li <904442+siutsin@users.noreply.github.com>
2025-05-11 01:01:12 +0100

    migrate to cloudflare provider 5 - tunnel (#1290)

--- infrastructure/modules/cloudflare-tunnel/00-outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cname" {
-  value     = cloudflare_zero_trust_tunnel_cloudflared.this.cname
-  sensitive = true
-}
--- infrastructure/modules/cloudflare-tunnel/01-tunnel.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudflare_zero_trust_tunnel_cloudflared" "this" {
-  account_id = var.account_id
-  name       = var.name
-  secret     = var.secret
-}
