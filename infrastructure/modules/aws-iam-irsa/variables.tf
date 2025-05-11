9eba640c816a1338b7cb9bd6f8822838157fb8b5
Simon Li <904442+siutsin@users.noreply.github.com>
2025-05-11 21:16:07 +0100

    add jung2bot irsa iam role (#1299)

---
+++ infrastructure/modules/aws-iam-irsa/variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  type = string
+}
+
+variable "oidc_provider" {
+  type = string
+}
+
+variable "service_account" {
+  type = string
+}
+
+variable "role_policies" {
+  type = map(object({
+    actions = list(string)
+    resources = list(string)
+  }))
+}
